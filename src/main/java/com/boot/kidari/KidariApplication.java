4ff37288689aa95d11f287fe957f5fb5d86c5ded
mark1q2w3e4r <ej10590@nate.com>
2022-07-05 21:59:21 +0900

    Project Add

---
+++ src/main/java/com/boot/kidari/KidariApplication.java
@@ -0,0 +1,13 @@
+package com.boot.kidari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidariApplication.class, args);
+	}
+
+}
